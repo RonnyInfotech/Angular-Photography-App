4c158baa48e62af4b95aa2dd841e095b187cc79b
Deep Ponkiya <deep@sharepointempower.com>
2020-05-23 10:40:17 +0530

    New Changes In Main.js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FourColumnGalleryComponent } from './four-column-gallery/four-column-gallery.component';
+import { PhotoDetailsComponent } from './photo-details/photo-details.component';
+import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
+import { TwoColumnGalleryComponent } from './two-column-gallery/two-column-gallery.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
+  { path: 'four-column-gallery', component: FourColumnGalleryComponent },
+  { path: 'two-column-gallery', component: TwoColumnGalleryComponent },
+  { path: 'photo-details', component: PhotoDetailsComponent },
+  { path: 'photo-gallery', component: PhotoGalleryComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
